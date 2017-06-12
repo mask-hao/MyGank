fa974bab54fd6aae64bce2ee664165aef12d1e2e
maskhao <281079145@qq.com>
2017-06-12 21:54:47 +0800

    1.修改自动更新，2.重新加载，3.关于页面

---
+++ app/src/main/java/com/zhanghao/gankio/ui/activity/DialogActivity.java
@@ -0,0 +1,10 @@
+package com.zhanghao.gankio.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhanghao on 2017/6/10.
+ */
+
+public class DialogActivity extends AppCompatActivity {
+}
